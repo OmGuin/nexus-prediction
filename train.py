e99f251877c81604b0091e21108961111229d73e
Om Guin <79595320+OmGuin@users.noreply.github.com>
2025-04-15 12:44:05 -0400

    Update train.py

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import pandas as pd
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+import pickle
 
 data = pd.read_csv('.csv')
 
@@ -12,11 +13,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 model = xgb.XGBRegressor(objective='reg:squarederror', n_estimators=100, learning_rate=0.1, max_depth=6)
 
-
 model.fit(X_train, y_train)
 
-y_pred = model.predict(X_test)
-
-
-mse = mean_squared_error(y_test, y_pred)
-print(f"Mean Squared Error: {mse}")
+with open("trained_xgb.pkl", "wb") as file:
+  pickle.dump(model, file)
