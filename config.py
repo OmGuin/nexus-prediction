e844aea4320cb82d1957a470c4e9479dff62e1d9
Om Guin <79595320+OmGuin@users.noreply.github.com>
2025-04-16 12:26:55 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,6 @@ class Config:
   def __init__(self):
     self.FEATURES = ["Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI", "DiabetesPedigreeFunction", "Age"]
     self.tunable_features = ["Glucose", "BMI"]
+    self.nontunable_features = ["BloodPressure", "SkinThickness", "Insulin", "DiabetesPedigreeFunction", "Age"]
+    self.bounds = [(80, 130), (18, 40)]
+    
