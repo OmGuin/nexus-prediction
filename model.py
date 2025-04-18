bb78c42919ba2b42a815766ee95eeab35f998adc
Om Guin <79595320+OmGuin@users.noreply.github.com>
2025-04-18 15:06:54 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 import torch.nn as nn
+from config import FEATURES
 
 class IRPredictor(nn.Module):
-    def __init__(self, input_dim = 0):
+    def __init__(self, input_dim = len(FEATURES)):
         super(IRPredictor, self).__init__()
         self.net = nn.Sequential(
             nn.Linear(input_dim, 64),
@@ -10,6 +11,14 @@ class IRPredictor(nn.Module):
             nn.ReLU(),
             nn.Linear(32,1),
             nn.Sigmoid()
+        )
+        self.net2 = nn.Sequential(
+            nn.Linear(input_dim, 4),
+            nn.Relu(),
+            nn.Linear(4, 2),
+            nn.Relu(),
+            nn.Linear(2,1),
+            nn.Relu()
         )
 
     def forward(self, x):
